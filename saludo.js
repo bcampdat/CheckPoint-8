a95070187a7ffa1adcd76fd393fdab8ce782a982
Bea <bcampdat@gmail.com>
2024-05-02 01:18:57 +0200

    .

---
+++ saludo.js
@@ -0,0 +1,18 @@
+"use strict";
+function variables() {
+    let nombre;
+
+    console.log(nombre);
+    console.log(typeof nombre);
+
+    console.log('hola');
+    console.log('adios');
+
+    nombre = 'Beatriz';
+
+    console.log(nombre);
+
+    nombre = prompt('Introduce tu nombre');
+
+    console.log(nombre);
+}
